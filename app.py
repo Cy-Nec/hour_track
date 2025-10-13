d9b556cac17334832e92361485f750918c3036a3
Cy_Nec <cy.nec01@gmail.com>
2025-10-13 17:39:51 +0500

    added gitignore and added ui

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from ui.mainWindow import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = QMainWindow()
+    window = MainWindow()
     window.show()
 
-    app.exec()
+    sys.exit(app.exec()) 
 
